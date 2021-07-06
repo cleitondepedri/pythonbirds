c6e07447ae5b4bc27b93dc3ed27511b9e7cea335
cleitondepedri <cleyton_depedri@hotmail.com>
2021-07-05 23:48:00 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,21 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Felipe')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Cleiton'
+    print(p.nome)
+
+
 
 
